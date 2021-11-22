19cdf87868be6755b0dbdba92c2e383fb2c10bfc
pierfrancescoscar <scarinci401@gmail.com>
2021-11-22 15:29:52 +0100

    ADD: hero vue component

---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="hero">
+      <h1>I'm my hero.</h1>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'Hero',
+}
+</script>
+
+<style lang="scss" scoped>
+
+.hero h1 {
+    font-size: 5rem;
+}
+
+.hero {
+    background-image: url('../assets/hand-painted-watercolor-background-with-sky-clouds-shape.jpg');
+    padding: 200px 100px;
+}
+
+</style>
